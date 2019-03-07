e0ff55bbe56dbfc6ef929701774ccd6142d5eb45
Ivan Lin <ilin00@stuy.edu>
2019-03-06 20:06:08 -0500

    Updated Skeletons

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -7,14 +7,14 @@ public class List_inArraySlots {
 
     // declare fields here
     public int[] refArray;
-    public int[] filledElements;
+    public int filledElements = 0;
 
 
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
-	refArray = new int[10];
+		refArray = new int[10];
     }
 
 
@@ -22,7 +22,7 @@ public class List_inArraySlots {
       @return the number of elements in this list
     */
     public int size() {
-	return refArray.length;
+		return refArray.length;
     }
 
 
@@ -31,35 +31,42 @@ public class List_inArraySlots {
        in [a,b,c,] format
       */ 
     public String toString() {
-	String array = "[";
-	for (int i = 0; i< refArray.length - 2 ; i++){
-	    array += refArray[i] + ",";
-	}
-	array += refArray[refArray.length - 1] + "]";
-	return array;   
+		String array = "[";
+		for (int i = 0; i< refArray.length - 1 ; i++){
+			array += refArray[i] + ",";
+		}
+		array += refArray[refArray.length - 1] + "]";
+		return array;   
     }
 
     
-    /** 
+    /* 
       Appends @value to the end of this list.
       
       @return true, in keeping with conventions yet to be discussed
      */
-     // public boolean add( int value) {
-     // }
+    public boolean add( int value) {
+		if (filledElements > refArray.length - 1) {
+			expand();
+		}
+		refArray[filledElements] = value;
+		filledElements++;
+		return true;
+	}
 
 
     /** 
       Double the capacity of the List_inArraySlots, 
       preserving existing data
      */
-     // private void expand() {
-        // System.out.println( "expand... (for debugging)");
-           // /* S.O.P. rules for debugging:
-              // Working methods should be silent. But during 
-              // development, the programmer must verify that 
-              // this method is called when that is appropriate.
-              // So test using the println(), then comment it out.
-              // */
-     // }
+    private void expand() {
+        System.out.println( "expand... (for debugging)");
+        int newCapacity = refArray.length * 2;
+		int[] newArray = new int [newCapacity];
+		for (int i = 0; i < refArray.length; i++){
+			newArray [i] = refArray [i];
+		}
+		refArray = new int[newCapacity];
+		refArray = newArray;
+    }
 }
